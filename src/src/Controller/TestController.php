118c199b240f74a756db670d9b9e094b19498c8f
KirillYarv <yarovoykirill942@gmail.com>
2026-02-08 21:03:37 +0300

    Добавление python решения в конфигурацию nginx и пожведение его под рекомендуемый для symfony. Правки docker-compose. Тестирование получения в контроллере сайта данных с api

--- src/src/Controller/TestController.php
+++ src/src/Controller/TestController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\DBAL\Connection;
@@ -12,7 +13,16 @@ final class TestController extends AbstractController
     #[Route('/test', name: 'app_test')]
     public function index(Connection $connection): Response
     {
-        dump($connection->isConnected());
+        $client = HttpClient::create();
+        $response = $client->request(
+            'GET',
+            'http://host.docker.internal:8081/ml/api/pairs'
+        );
+
+        $statusCode = $response->getStatusCode();
+        $contentType = $response->getHeaders()['content-type'][0];
+        $content = $response->toArray();
+        dump($content);
         return $this->render('test/index.html.twig', [
             'controller_name' => 'TestController',
         ]);
